17ac219b8050dd2798187a06043df35735bee44c
FreeFly0125 <simonbergman516@gmail.com>
2024-02-29 09:06:29 +0100

    add station when create new line
    Co-authored-by: FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>

--- src/services/trainline.service.ts
+++ src/services/trainline.service.ts
@@ -1,30 +1,42 @@
-import { TrainLineEntity } from "entities";
-import { getLineRepository } from "repositories";
+import { StationEntity, TrainLineEntity } from "entities";
+import { getLineRepository, getStationRepository } from "repositories";
 
 export const createLineService = async (name: string, stations: string[]) => {
   const lineRepository = await getLineRepository();
+  const stationRepository = await getStationRepository();
 
   const newLine = new TrainLineEntity();
   newLine.name = name;
+
+  const stationEntities = await Promise.all(
+    stations.map(async (stationName) => {
+      let station = await stationRepository.findOne({ where: { name: stationName }});
+      if (!station) {
+        station = stationRepository.create({ name: stationName });
+        await stationRepository.save(station);
+      }
+      return station;
+    })
+  );
+
   newLine.stations = stations;
 
   const line = await lineRepository.save(newLine);
-
   return line;
 };
 
 export const getLinesService = async () => {
   const lineRepository = await getLineRepository();
 
-  const lines = lineRepository.find({ order: { id: 'ASC' } });
+  const lines = lineRepository.find({ order: { id: "ASC" } });
 
   return lines;
 };
 
 export const getLineService = async (id: number) => {
-    const lineRepository = await getLineRepository();
-  
-    const lines = lineRepository.findOne({ where: { id: id } });
-  
-    return lines;
-  };
+  const lineRepository = await getLineRepository();
+
+  const lines = lineRepository.findOne({ where: { id: id } });
+
+  return lines;
+};
