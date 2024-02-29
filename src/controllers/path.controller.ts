1ed5fb63f80b4ff40584c39962007f1abd5c9051
FreeFly0125 <simonbergman516@gmail.com>
2024-02-29 14:12:36 +0100

    search route from origin to dest
    Co-authored-by: FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>

--- src/controllers/path.controller.ts
+++ src/controllers/path.controller.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { getNeighborStationService } from "services/station.service";
 
-export const searchPath = (req: Request, res: Response) => {
-  console.log("Search Path!");
+export const searchPath = async (req: Request, res: Response) => {
+  let stations = await getNeighborStationService();
+
+  const { origin, destination } = req.query;
+  const originStation = origin as string;
+  const destStation = destination as string;
+
+  const searchPath = (path: string[]): string[] | null => {
+    if (path[path.length - 1] === destStation) {
+      return path;
+    }
+    const curStation = stations.find(
+      (station) => station.name === path[path.length - 1]
+    );
+    for (const neiStation of curStation.neighbors) {
+      if (!path.includes(neiStation)) {
+        path.push(neiStation);
+        const result = searchPath(path);
+        if (result) { return result; }
+        path.pop();
+      }
+    }
+    return null;
+  };
+
+  const path = searchPath([originStation]);
+
+  res.status(httpStatus.OK).send(path);
 };
--- src/services/station.service.ts
+++ src/services/station.service.ts
@@ -1,33 +1,47 @@
 import { getCardRepository, getStationRepository } from "repositories";
 
+export const getNeighborStationService = async () => {
+  const stationRepository = await getStationRepository();
+  const stations = await stationRepository.find({select: ['name', 'neighbors']});
+  return stations
+};
+
 export const getStationsService = async () => {
-    const stationRepository = await getStationRepository();
-    const stations = stationRepository.find({ order: { id: 'ASC' } });
-    return stations;
+  const stationRepository = await getStationRepository();
+  const stations = stationRepository.find({ order: { id: "ASC" } });
+  return stations;
 };
 
 export const getStationService = async (id: number) => {
-    const stationRepository = await getStationRepository();
-    const station = stationRepository.findOne({ where: { id: id } });
-    return station
+  const stationRepository = await getStationRepository();
+  const station = stationRepository.findOne({ where: { id: id } });
+  return station;
 };
 
-export const enterStationService = async (stationName: string, number: string) => {
-    const stationRepository = await getStationRepository();
-    const cardRepository = await getCardRepository();
+export const enterStationService = async (
+  stationName: string,
+  number: string
+) => {
+  const stationRepository = await getStationRepository();
+  const cardRepository = await getCardRepository();
 
-    const station = await stationRepository.findOne({ where: {name: stationName} });
-    const card = await cardRepository.findOne({ where: { number: number } });
-    card.riding = true;
-    card.amount -= station.fare;
-    return cardRepository.save(card);
+  const station = await stationRepository.findOne({
+    where: { name: stationName },
+  });
+  const card = await cardRepository.findOne({ where: { number: number } });
+  card.riding = true;
+  card.amount -= station.fare;
+  return cardRepository.save(card);
 };
 
-export const exitStationService = async (stationName: string, number: string) => {
-    const stationRepository = await getStationRepository();
-    const cardRepository = await getCardRepository();
+export const exitStationService = async (
+  stationName: string,
+  number: string
+) => {
+  const stationRepository = await getStationRepository();
+  const cardRepository = await getCardRepository();
 
-    const card = await cardRepository.findOne({ where: { number: number } });
-    card.riding = false;
-    return cardRepository.save(card);
+  const card = await cardRepository.findOne({ where: { number: number } });
+  card.riding = false;
+  return cardRepository.save(card);
 };
